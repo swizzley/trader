2cb54efd6b01cf3a1742d6789e79f76dfd34ac93
Dustin Morgan <contact@dustinmorgan.org>
2026-02-13 14:38:21 -0700

    fix orders

--- accounts-trading.go
+++ accounts-trading.go
@@ -28,51 +28,341 @@ import (
 )
 
 var (
-	accountEndpoint        string = "https://api.schwabapi.com/trader/v1"
+	accountEndpoint string = "https://api.schwabapi.com/trader/v1"
+
+	// Accounts
 	endpointAccountNumbers string = accountEndpoint + "/accounts/accountNumbers"
 	endpointAccounts       string = accountEndpoint + "/accounts"
 	endpointAccount        string = accountEndpoint + "/accounts/%s"
 	// endpointUserPreference string = accountEndpoint + "/userPreference"
+
+	// Orders
 	endpointOrders        string = accountEndpoint + "/orders"
 	endpointAccountOrders string = accountEndpoint + "/accounts/%s/orders"
 	endpointAccountOrder  string = accountEndpoint + "/accounts/%s/orders/%s"
 	// endpointPreviewOrder  string = accountEndpoint + "/accounts/%s/previewOrder"
+
+	// Transactions
 	// endpointTransactions string = accountEndpoint + "/accounts/%s/transactions"
 	endpointTransaction string = accountEndpoint + "/accounts/%s/transactions/%s"
+)
 
-	OrderTemplate = `
-{
-  "orderType": "%s",
-  "session": "%s",
-  "duration": "%s",
-  "orderStrategyType": "%s",
-  "orderLegCollection": [
-    %s
-  ]
+type Transaction struct {
+	ActivityId     int
+	Time           string
+	User           User
+	Description    string
+	AccountNumber  string
+	Type           string
+	Status         string
+	SubAccount     string
+	TradeDate      string
+	SettlementDate string
+	PositionId     int
+	OrderId        int
+	NetAmount      int
+	ActivityType   string
+	TransferItems  TransferItems
 }
-`
 
-	LegTemplate = `
-{
-  "instruction": "%s",
-  "quantity": %f,
-  "instrument": {
-    "symbol": "%s",
-    "assetType": "%s"
-  }
-},
-`
+type User struct {
+	CdDomainId     string
+	Login          string
+	Type           string
+	UserId         int
+	SystemUserName string
+	FirstName      string
+	LastName       string
+	BrokerRepCode  string
+}
 
-	LegTemplateLast = `
-{
-  "instruction": "%s",
-  "quantity": %f,
-  "instrument": {
-    "symbol": "%s",
-    "assetType": "%s"
-  }
-},
-`
+type TransferItems struct {
+	Instrument     InstrumentRef
+	Amount         int
+	Cost           int
+	Price          float64
+	FeeType        string
+	PositionEffect string
+}
+
+type InstrumentRef struct {
+	Cusip        string
+	Symbol       string
+	Description  string
+	InstrumentId int
+	NetChange    int
+	Type         string
+}
+
+type AccountNumbers struct {
+	AccountNumber string
+	HashValue     string
+}
+
+type Account struct {
+	SecuritiesAccount SecuritiesAccount
+	AggregatedBalance AggregatedBalance
+}
+
+type SecuritiesAccount struct {
+	Type                    string
+	AccountNumber           string
+	RoundTrips              int
+	IsDayTrader             bool
+	IsClosingOnlyRestricted bool
+	PFCBFlag                bool
+	Positions               []Position
+	InitialBalances         InitialBalance
+	CurrentBalances         CurrentBalance
+	ProjectedBalances       ProjectedBalance
+}
+
+type Position struct {
+	ShortQuantity                  float64
+	AveragePrice                   float64
+	CurrentDayProfitLoss           float64
+	CurrentDayProfitLossPercentage float64
+	LongQuantity                   float64
+	SettledLongQuantity            float64
+	SettledShortQuantity           float64
+	AgedQuantity                   float64
+	Instrument                     AccountInstrument
+	MarketValue                    float64
+	MaintenanceRequirement         float64
+	AverageLongPrice               float64
+	AverageShortPrice              float64
+	TaxLotAverageLongPrice         float64
+	TaxLotAverageShortPrice        float64
+	LongOpenProfitLoss             float64
+	ShortOpenProfitLoss            float64
+	PreviousSessionLongQuantity    float64
+	PreviousSessionShortQuantity   float64
+	CurrentDayCost                 float64
+}
+
+type AccountInstrument struct {
+	AssetType    string
+	Cusip        string
+	Symbol       string
+	Description  string
+	InstrumentID int
+	NetChange    float64
+}
+
+type InitialBalance struct {
+	AccruedInterest                  float64
+	AvailableFundsNonMarginableTrade float64
+	BondValue                        float64
+	BuyingPower                      float64
+	CashBalance                      float64
+	CashAvailableForTrading          float64
+	CashReceipts                     float64
+	DayTradingBuyingPower            float64
+	DayTradingBuyingPowerCall        float64
+	DayTradingEquityCall             float64
+	Equity                           float64
+	EquityPercentage                 float64
+	LiquidationValue                 float64
+	LongMarginValue                  float64
+	LongOptionMarketValue            float64
+	LongStockValue                   float64
+	MaintenanceCall                  float64
+	MaintenanceRequirement           float64
+	Margin                           float64
+	MarginEquity                     float64
+	MoneyMarketFund                  float64
+	MutualFundValue                  float64
+	RegTCall                         float64
+	ShortMarginValue                 float64
+	ShortOptionMarketValue           float64
+	ShortStockValue                  float64
+	TotalCash                        float64
+	IsInCall                         bool
+	UnsettledCash                    float64
+	PendingDeposits                  float64
+	MarginBalance                    float64
+	ShortBalance                     float64
+	AccountValue                     float64
+}
+
+/*type CurrentBalance struct {
+	AvailableFunds                   float64
+	AvailableFundsNonMarginableTrade float64
+	BuyingPower                      float64
+	BuyingPowerNonMarginableTrade    float64
+	DayTradingBuyingPower            float64
+	DayTradingBuyingPowerCall        float64
+	Equity                           float64
+	EquityPercentage                 float64
+	LongMarginValue                  float64
+	MaintenanceCall                  float64
+	MaintenanceRequirement           float64
+	MarginBalance                    float64
+	RegTCall                         float64
+	ShortBalance                     float64
+	ShortMarginValue                 float64
+	SMA                              float64
+	IsInCall                         float64
+	StockBuyingPower                 float64
+	OptionBuyingPower                float64
+}*/
+
+type CurrentBalance struct {
+	AccruedInterest                  float64
+	CashBalance                      float64
+	CashReceipts                     float64
+	LongOptionMarketValue            float64
+	LiquidationValue                 float64
+	LongMarketValue                  float64
+	MoneyMarketFund                  float64
+	Savings                          float64
+	ShortMarketValue                 float64
+	PendingDeposits                  float64
+	MutualFundValues                 float64
+	BondValue                        float64
+	ShortOptionMarketValue           float64
+	AvailableFunds                   float64
+	AvailableFundsNonMarginableTrade float64
+	BuyingPower                      float64
+	BuyingPowerNonMarginableTrade    float64
+	DayTradingBuyingPower            float64
+	Equity                           float64
+	EquityPercentage                 float64
+	LongMarginValue                  float64
+	MaintenanceCall                  float64
+	MaintenanceRequirement           float64
+	MarginBalance                    float64
+	RegTCall                         float64
+	ShortBalance                     float64
+	ShortMarginValue                 float64
+	SMA                              float64
+}
+
+type ProjectedBalance struct {
+	AvailableFunds                   float64
+	AvailableFundsNonMarginableTrade float64
+	BuyingPower                      float64
+	BuyingPowerNonMarginableTrade    float64
+	DayTradingBuyingPower            float64
+	DayTradingBuyingPowerCall        float64
+	Equity                           float64
+	EquityPercentage                 float64
+	LongMarginValue                  float64
+	MaintenanceCall                  float64
+	MaintenanceRequirement           float64
+	MarginBalance                    float64
+	RegTCall                         float64
+	ShortBalance                     float64
+	ShortMarginValue                 float64
+	SMA                              float64
+	IsInCall                         bool
+	StockBuyingPower                 float64
+	OptionBuyingPower                float64
+}
+
+type AggregatedBalance struct {
+	CurrentLiquidationValue float64
+	LiquidationValue        float64
+}
+
+type FullOrder struct {
+	Session                  string
+	Duration                 string
+	OrderType                string
+	CancelTime               string
+	ComplexOrderStrategyType string
+	Quantity                 float64
+	FilledQuantity           float64
+	RemainingQuantity        float64
+	RequestedDestination     string
+	DestinationLinkName      string
+	ReleaseTime              string
+	StopPrice                float64
+	StopPriceLinkBasis       string
+	StopPriceLinkType        string
+	StopPriceOffset          float64
+	StopType                 string
+	Price                    float64
+	TaxLotMethod             string
+	OrderLegCollection       []FullOrderLeg
+	ActivationPrice          float64
+	SpecialInstruction       string
+	OrderStrategyType        string
+	OrderId                  int
+	Cancelable               bool
+	Editable                 bool
+	Status                   string
+	EnteredTime              string
+	CloseTime                string
+	Tag                      string
+	AccountNumber            int
+	OrderActivityCollection  []FullOrderActivity
+	ReplacingOrderCollection string
+	ChildOrderStrategies     string
+	StatusDescription        string
+}
+
+type FullOrderActivity struct {
+	ActivityType           string
+	ExecutionType          string
+	Quantity               float64
+	OrderRemainingQuantity float64
+	ExecutionLegs          []FullExecutionLeg
+}
+
+type FullExecutionLeg struct {
+	LegId             int
+	Price             float64
+	Quantity          float64
+	MismarkedQuantity float64
+	InstrumentId      int
+	Time              string
+}
+
+type FullOrderLeg struct {
+	OrderLegType   string
+	LegId          int
+	Instrument     InstrumentRef
+	Instruction    string
+	PositionEffect string
+	Quantity       float64
+	QuantityType   string
+	DivCapGains    string
+	ToSymbol       string
+}
+
+type SingleLegOrder struct {
+	OrderType   string `default:"MARKET"`
+	Session     string `default:"NORMAL"`
+	Duration    string `default:"DAY"`
+	Strategy    string `default:"SINGLE"`
+	Instruction string
+	Quantity    float32
+	Instrument  SimpleOrderInstrument
+}
+
+type MultiLegOrder struct {
+	OrderType          string // LIMIT, MARKET
+	Session            string // NORMAL
+	Duration           string // DAY
+	Strategy           string // SINGLE
+	OrderLegCollection []SimpleOrderLeg
+}
+
+type SimpleOrderLeg struct {
+	Instruction string
+	Quantity    float32
+	Instrument  SimpleOrderInstrument
+}
+
+type SimpleOrderInstrument struct {
+	Symbol    string
+	AssetType string // EQUITY
+}
+
+type (
+	SingleLegOrderComposition      func(order *SingleLegOrder)
+	MultiLegSimpleOrderComposition func(order *MultiLegOrder)
 )
 
 // Create a new Market order
@@ -132,6 +422,40 @@ func Instrument(instrument SimpleOrderInstrument) SingleLegOrderComposition {
 		order.Instrument = instrument
 	}
 }
+
+var OrderTemplate = `
+{
+  "orderType": "%s",
+  "session": "%s",
+  "duration": "%s",
+  "orderStrategyType": "%s",
+  "orderLegCollection": [
+    %s
+  ]
+}
+`
+
+var LegTemplate = `
+{
+  "instruction": "%s",
+  "quantity": %f,
+  "instrument": {
+    "symbol": "%s",
+    "assetType": "%s"
+  }
+},
+`
+
+var LegTemplateLast = `
+{
+  "instruction": "%s",
+  "quantity": %f,
+  "instrument": {
+    "symbol": "%s",
+    "assetType": "%s"
+  }
+},
+`
 
 func marshalSingleLegOrder(order *SingleLegOrder) string {
 	return fmt.Sprintf(OrderTemplate, order.OrderType, order.Session, order.Duration, order.Strategy, fmt.Sprintf(LegTemplate, order.Instruction, order.Quantity, order.Instrument.Symbol, order.Instrument.AssetType))
@@ -216,7 +540,7 @@ func (agent *Agent) GetAccountOrders(accountNumber, fromEnteredTime, toEnteredTi
 	return orders, nil
 }
 
-// WIP: do not use
+// WIP:
 // fromEnteredTime, toEnteredTime format:
 // yyyy-MM-ddTHH:mm:ss.SSSZ
 func (agent *Agent) GetAllOrders(fromEnteredTime, toEnteredTime string) ([]FullOrder, error) {
@@ -238,11 +562,10 @@ func (agent *Agent) GetAllOrders(fromEnteredTime, toEnteredTime string) ([]FullO
 		return []FullOrder{}, err
 	}
 	var orders []FullOrder
+	/* TODO:
 	err = sonic.Unmarshal(body, &orders)
-	if err != nil {
-		fmt.Println(body)
-		return []FullOrder{}, err
-	}
+	isErrNil(err)*/
+	fmt.Println(body)
 	return orders, nil
 }
 
